c46badbf9257b8b194dee635106087f5df2f31eb
RazelLaminoza <laminozarazel13@gmail.com>
2026-01-17 22:07:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ if st.session_state.page == "landing":
     )
 
     # Register button centered neatly below text
-    col1, col2, col3 = st.columns([4,1,4])
+    col1, col2, col3 = st.columns([5,1,5])
     with col2:
         if st.button("Register"):
             st.session_state.page = "register"
