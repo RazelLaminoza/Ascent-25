1d5ff4569741efe7a2e1a9d075b17b068d1c0b32
RazelLaminoza <laminozarazel13@gmail.com>
2026-01-18 10:24:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -206,7 +206,12 @@ if st.session_state.page == "landing":
     st.markdown(
         f"""
         <div style='height:100vh; display:flex; flex-direction:column; justify-content:center; align-items:center; text-align:center;'>
-            ...
+            <img src='data:image/png;base64,{base64.b64encode(open("2.png","rb").read()).decode()}' width='160'/>
+            <img src='data:image/png;base64,{base64.b64encode(open("1.png","rb").read()).decode()}' style='width:70%; max-width:900px; margin-top:20px;'/>
+            <p style="font-size:18px;">
+                PRE-REGISTER NOW AND TAKE PART IN THE RAFFLE<br>
+                <span style="font-size:16px;">January 25, 2026 | OKADA BALLROOM 1–3</span>
+            </p>
         </div>
         """,
         unsafe_allow_html=True
@@ -221,6 +226,7 @@ if st.session_state.page == "landing":
         st.markdown("<div style='height: 10px'></div>", unsafe_allow_html=True)
         if st.button("Register Here"):
             go_to("register")
+
 
 
 
