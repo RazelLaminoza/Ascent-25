387c3dad67103da6ab70d61ca1bb60f3a8a64c97
RazelLaminoza <laminozarazel13@gmail.com>
2026-01-18 22:56:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -311,10 +311,12 @@ if st.session_state.page == "landing":
             <img src='data:image/png;base64,{base64.b64encode(open("2.png","rb").read()).decode()}' width='160'/>
             <img src='data:image/png;base64,{base64.b64encode(open("1.png","rb").read()).decode()}' style='width:70%; max-width:900px; margin-top:20px;'/>
 
-            <p style="font-size:18px;">
-                PRE-REGISTER NOW AND TAKE PART IN THE RAFFLE<br>
-                <span style="font-size:16px;">January 25, 2026 | OKADA BALLROOM 1–3</span>
-            </p>
+           <p style="font-size:18px; text-align:center; margin-top:20px; line-height:1.3;">
+    PRE-REGISTER NOW AND TAKE PART IN THE RAFFLE<br>
+    <span style="font-size:16px;">
+        January 25, 2026 | OKADA BALLROOM 1–3
+    </span>
+</p>
         </div>
         """,
         unsafe_allow_html=True
