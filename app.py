818641f4e51d910b101b0baf39e23aa46316ee86
RazelLaminoza <laminozarazel13@gmail.com>
2026-01-20 00:21:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -333,6 +333,11 @@ if st.session_state.page == "landing":
         border-radius: 8px;
         margin-top: 20px;
     }
+
+    /* hide the streamlit button */
+    #hidden-btn {
+        display: none;
+    }
     </style>
     """, unsafe_allow_html=True)
 
@@ -345,22 +350,15 @@ if st.session_state.page == "landing":
                 PRE-REGISTER NOW AND TAKE PART IN THE RAFFLE<br>
                 <span style="font-size:16px;">January 25, 2026 | OKADA BALLROOM 1–3</span>
             </p>
-            <button class="landing-btn" onclick="window.location.href='#'">Pre-register</button>
+
+            <button class="landing-btn" onclick="document.getElementById('hidden-btn').click()">
+                Pre-register
+            </button>
         </div>
         """,
         unsafe_allow_html=True
     )
 
-   # ---- STREAMLIT BUTTON (separate, NOT inside markdown) ----
-    col1, col2, col3 = st.columns([1, 2, 1])
-    with col2:
-        st.button(
-            "Pre-register",
-            on_click=go_to,
-            args=("register",),
-            type="primary",
-            key="landing_register_1"
-        )
 
 # ---------------- REGISTER ----------------
 elif st.session_state.page == "register":
